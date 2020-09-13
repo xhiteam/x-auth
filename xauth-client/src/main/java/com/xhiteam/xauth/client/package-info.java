d9ecaac5940752fb942537fcb6ba06e89de4c402
Lambo Chen <lambo.chen.2306@gmail.com>
2020-09-13 21:17:14 +0800

    impl-jwt, client

---
+++ xauth-client/src/main/java/com/xhiteam/xauth/client/package-info.java
@@ -0,0 +1,7 @@
+package com.xhiteam.xauth.client;
+/**
+ * @author lambo.chen.2306@gmail.com
+ * @date 2020/9/13 21:14
+ *
+ * XAuth 客户端
+ **/
---
+++ xauth-impl-jwt/src/main/java/com/xhiteam/xauth/impl/jwt/package-info.java
@@ -0,0 +1,7 @@
+package com.xhiteam.xauth.impl.jwt;
+/**
+ * @author lambo.chen.2306@gmail.com
+ * @date 2020/9/13 21:11
+ *
+ * X-Auth 采用 JWT 实现
+ **/
