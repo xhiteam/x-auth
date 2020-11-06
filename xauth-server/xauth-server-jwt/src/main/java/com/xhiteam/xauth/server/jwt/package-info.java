0158f1463bb89b45a3f0e2e997a1e4517738a556
Lambo Chen <lambo.chen.2306@gmail.com>
2020-11-06 22:32:18 +0800

    1.0 概述

--- xauth-server/xauth-server-jwt/src/main/java/com/xhiteam/xauth/server/jwt/package-info.java
+++ xauth-server/xauth-server-jwt/src/main/java/com/xhiteam/xauth/server/jwt/package-info.java
@@ -4,5 +4,18 @@
  * @since version 1.0
  *
  * 此 module 主要进行 JWT 实现 x-auth 功能的认证中心、后台管理平台研发。
+ *
+ * 【认证中心】RESTful API:
+ * 1. token 签发
+ * 2. token 解析
+ * 3. token 刷新
+ *
+ * 【后台管理】
+ * 1. token 管理（查看，删除）
+ *
+ * xauth token 机制
+ * access_token: xauth客户端存储
+ * data_token：服务端存储
+ *
  **/
 package com.xhiteam.xauth.server.jwt;
