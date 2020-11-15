99ed8f91dabdbcde0e95b22a2ae6ebde2f4a9d40
MrWater233 <327191248@qq.com>
2020-11-15 20:02:01 +0800

    完善example和README

--- README.md
+++ README.md
@@ -18,9 +18,23 @@
 
    可以将用户的角色、权限信息导入并生成相应的JWT-Token
 
-   ```java
-   token = tokenRepository.newToken(String.valueOf(user.getId()));
-   ```
+   - 只设置用户的id（实体的唯一标识）
+   
+     ```java
+     token = tokenRepository.newToken(String.valueOf(user.getId()));
+     ```
+   
+   - 设置用户的id、角色、权限
+   
+     ```java
+     ArrayList<String> permissions = new ArrayList<>();
+     permissions.add("user:getAll");
+     ArrayList<String> roles = new ArrayList<>();
+     roles.add("admin");
+     token = tokenRepository.newToken(String.valueOf(user.getId()), roles, permissions, null);
+     ```
+   
+   除了这些基本的设置，还可以设置token超时时间、需要token携带的额外信息等，这些都在`tokenRepository`中进行了重载。
    
 3. 使用相关注解对接口进行权限校验
 
--- xauth-examples/xauth-example-jwt/src/main/java/com/xhiteam/xauth/example/jwt/controller/UserController.java
+++ xauth-examples/xauth-example-jwt/src/main/java/com/xhiteam/xauth/example/jwt/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.xhiteam.xauth.example.jwt.controller;
 
 import com.xhiteam.xauth.core.annotation.Ignore;
+import com.xhiteam.xauth.core.annotation.RequiresPermissions;
+import com.xhiteam.xauth.core.annotation.RequiresRoles;
 import com.xhiteam.xauth.core.model.Token;
 import com.xhiteam.xauth.core.repository.TokenRepository;
 import com.xhiteam.xauth.example.jwt.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author WanJingmiao
@@ -24,7 +30,7 @@ public class UserController {
 	TokenRepository tokenRepository;
 
 	/**
-	 * 登录示例
+	 * 模拟登录
 	 * 通过 @Ignore注解来忽略JWT验证
 	 * 下一次请求时使用Authorization - tokenStr来实现JWT认证
 	 *
@@ -38,13 +44,19 @@ public class UserController {
 		if ("张三".equals(user.getUsername()) && "123456".equals(user.getPassword())) {
 			// 这里假设数据库中查询的用户id为1
 			user.setId(1L);
-			token = tokenRepository.newToken(String.valueOf(user.getId()));
+			// 用户的权限
+			ArrayList<String> permissions = new ArrayList<>();
+			permissions.add("user:getAll");
+			// 用户的角色
+			ArrayList<String> roles = new ArrayList<>();
+			roles.add("admin");
+			token = tokenRepository.newToken(String.valueOf(user.getId()), roles, permissions, null);
 		}
 		return token.getTokenStr();
 	}
 
 	/**
-	 * 获取用户信息
+	 * 模拟获取用户信息
 	 * 默认开启JWT认证
 	 *
 	 * @param id 用户id
@@ -56,5 +68,39 @@ public class UserController {
 		user.setId(id);
 		user.setUsername("张三");
 		return user;
+	}
+
+	/**
+	 * 模拟获取所有用户信息
+	 * 需要user:getAll权限
+	 *
+	 * @return 所有用户信息
+	 */
+	@GetMapping("/list")
+	@RequiresPermissions({"user:getAll"})
+	public List<User> getUserList() {
+		ArrayList<User> users = new ArrayList<>();
+		User user1 = new User();
+		User user2 = new User();
+		user1.setId(1L);
+		user1.setUsername("张三");
+		user2.setId(2L);
+		user2.setUsername("李四");
+		users.add(user1);
+		users.add(user2);
+		return users;
+	}
+
+	/**
+	 * 模拟删除用户
+	 * 需要admin权限
+	 *
+	 * @param id 用户id
+	 * @return 删除的提示信息
+	 */
+	@DeleteMapping
+	@RequiresRoles({"admin"})
+	public String deleteUser(Long id) {
+		return "删除成功";
 	}
 }
