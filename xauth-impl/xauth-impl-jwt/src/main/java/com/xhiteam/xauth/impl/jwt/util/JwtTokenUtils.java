3b32fb8a756e162cf752340a4fec779b24f03f63
MrWater233 <327191248@qq.com>
2020-11-16 17:08:56 +0800

    去除多余import

--- xauth-impl/xauth-impl-jwt/src/main/java/com/xhiteam/xauth/impl/jwt/util/JwtTokenUtils.java
+++ xauth-impl/xauth-impl-jwt/src/main/java/com/xhiteam/xauth/impl/jwt/util/JwtTokenUtils.java
@@ -16,7 +16,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.util.StringUtils;
 
-import javax.annotation.PostConstruct;
 import java.security.Key;
 import java.util.*;
 
